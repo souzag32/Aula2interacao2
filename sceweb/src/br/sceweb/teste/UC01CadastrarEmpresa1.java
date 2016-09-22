14be792cc75d37e6c3aed3e2399896bd00ad3a32
edson <engsoftware1@gmail.com>
2016-09-22 13:56:00 -0300

    UC04CadastrarEmpresa e UC05ExcluirEmpresa - manutençao evolutiva
    inclusao das funcionalidades de cadastro e exclusao de empresa

--- sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa1.java
+++ sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa1.java
@@ -3,22 +3,43 @@ package br.sceweb.teste;
 import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.sceweb.modelo.Empresa;
+import br.sceweb.modelo.EmpresaDAO;
+
 public class UC01CadastrarEmpresa1 {
 
+	static EmpresaDAO empresaDAO;
+	static Empresa empresa;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
+	}
+	/**
+	 * estabelece as pre-condicoes antes da execucao de cada teste
+	 * @throws Exception
+	 */
+	@Before
+	public void excluiEmpresa() throws Exception{
+		empresaDAO.exclui("89424232000180");
 	}
 
+	@Test
+	public void CT01UC01FBCadastra_com_sucesso() {
+		assertEquals(1,empresaDAO.adiciona(empresa));
+	}
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
